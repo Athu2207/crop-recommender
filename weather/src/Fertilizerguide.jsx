b4ae53010a998c57d83cf3098e370f251e7469c5
Atharva <atharvalole12@gmail.com>
2025-06-29 01:24:15 +0530

    removed use

--- weather/src/Fertilizerguide.jsx
+++ weather/src/Fertilizerguide.jsx
@@ -1,4 +1,4 @@
-import React, { use, useState } from 'react'
+import React, { useState } from 'react'
 import "./Fertilizerguide.css"
 function Fertilizerguide() {
     const [nitrovalue,setNitrovalue]=useState("")
