55a5e885faa294ccfdaf0c1e20740e1866c3a9c1
Atharva <atharvalole12@gmail.com>
2025-07-04 18:16:36 +0530

    removed useeffect

--- weather/src/MainPage.jsx
+++ weather/src/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState } from 'react';
 import './MainPage.css';
 import { useNavigate } from 'react-router-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
